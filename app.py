247c9e4c212c7c1212be6a44b33f34b8208ca846
Kunal <kunaldhiman27971@gmail.com>
2026-07-10 09:12:24 +0530

    workflow action

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ def sum(a:int,b:int)-> int:
 def subtract(a:int,b:int)-> int:
     return a-b
 
+
+
+
+
+# jb user ne vscode se github me push kiya hai ise hm event kehte hai is event kebaad ek code chalega isi ko workflow kehte hai
+
+
+# yml file ka kaam hai github ko batana ki jb user ne push kiya hai to kya kya karna hai ye yml file me likha hota hai use ek environment provide krana.
